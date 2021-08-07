71637122686d146deb6862cb80265db767fc4fe1
Sharath V <sharathkrml@gmail.com>
2021-08-07 21:26:36 +0530

    Object in ES6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-//const ->scope = block(same as let)
-const x=1;
-x=2;  //cannot reassign const
+//person is an object = key value pairs
+//function inside object is called method
+//walk is a method in person object
+const person = {
+  name:'Sharath',
+  walk:function(){},
+  talk(){console.log(this.name)}//es6 syntax for method 
+};
+//2 ways to access members
+
+//bracket notation  --if we are unsure about object members
+const targetMember='names';
+person[targetMember] = 'Sharath 💯';
+//dot notation
+person.talk();
