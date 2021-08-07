6242e2730b87dfaf089e3af757aadfb29ea1c5ca
Sharath V <sharathkrml@gmail.com>
2021-08-07 20:02:59 +0530

    difference between var and let

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// var -> Scope == Function
+// let -> Scope == Block in which its defined
+// const -> Scope == Block in which its defined
+function sayHello(){
+  for (let i=0;i<5;i++){
+    console.log(i);
+  }
+  console.log(i) //if var,i=5  if let,i not defined
+}
+sayHello();
