0ea5bb4fa59e3a7f4cb4faf9b0a4ec87eea5097e
Sharath V <sharathkrml@gmail.com>
2021-08-07 21:49:44 +0530

    binding object to a standalone function

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ const person = {
     console.log(this);
   }
 };
-//if this is called as menthod in an object,this references the the object
-person.walk()
-//if called funstion outside object,this references window object
-const walk = person.walk;
-walk()
+//if called function outside object,this references window object
+// calling a function as standalone function
+const oldwalk = person.walk;
+oldwalk();
+//in JS,every function is an object,they have properties and methods we can use
+//with bind method,we can set value of this permanently
+const newwalk = person.walk.bind(person);
+newwalk();
+//when we set bind with walk function,we get a new walk function
+// in newwalk function,value of this is based on argument passed to bind method 
+// new walk function is always attached to person object
