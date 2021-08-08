e457b354de4a521537c4a5a4ff94fdd9dd110bc3
Sharath V <sharathkrml@gmail.com>
2021-08-08 11:20:36 +0530

    Classes

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-const first = [1,2,3]
-const second = [4,5,6]
-
-//concatenate 2 arrays
-const combined = first.concat(second)
-console.log(combined)
-//using spread operator
-// ...first gets all items from first array
-const newcombined = [...first,...second,...combined]
-console.log(newcombined)
-// spread is useful eg:- we can add element in between
-
-const l = [...first,'a',...second]
-console.log(l)
-//if old way
-const a = first.concat('a')
-const b = a.concat(second)
-console.log(b)
-//clone array
-const clone = [...first]
-console.log(first)
-console.log(clone)
-//we can also apply spread operator on objects
-const fobject = {name:'sharath'};
-const sobject = {age:21};
-const cobject = {...fobject,...sobject,place:'kerala'};
-console.log(cobject);
+//we have person object
+const person={
+  name:'Sharath',
+  walk(){
+    console.log('walk')
+  }
+};
+//if we want another person object
+const person2={
+  name:'Sharath2',
+  walk(){
+    console.log('walk')
+  }
+};
+//if implementaion of walk has any issues,debugging is complex
+//when we have an object with atleast one method,we need a blueprint to create objects of that type
+//class solves the problem
+class Person{
+  //special method called constructor
+  constructor(name,age){
+    this.name=name;
+    this.age=age;
+  }
+  walk(){
+    console.log('walk')
+  }
+}
+//sharath object of Person Class
+const sharath = new Person('sharath',21)
+console.log(sharath)
+sharath.walk()
